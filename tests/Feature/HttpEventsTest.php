2205ade8456f6de5f0cc0f137532b35f65c2d21f
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-03-05 19:31:43 +0000

    Added test for request pools

--- tests/Feature/HttpEventsTest.php
+++ tests/Feature/HttpEventsTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Saloon\Contracts\Response;
 use Saloon\HttpSender\HttpSender;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Config;
@@ -16,6 +17,8 @@ test('the http events are fired when using the http sender', function () {
     Event::fake();
 
     $connector = new HttpSenderConnector;
+
+    expect($connector->sender())->toBeInstanceOf(HttpSender::class);
 
     $responseA = $connector->send(new UserRequest);
     $responseB = $connector->send(new UserRequest);
@@ -36,6 +39,8 @@ test('the http events are fired when using the http sender with asynchronous eve
 
     $connector = new HttpSenderConnector;
 
+    expect($connector->sender())->toBeInstanceOf(HttpSender::class);
+
     $responseA = $connector->sendAsync(new UserRequest)->wait();
     $responseB = $connector->sendAsync(new UserRequest)->wait();
     $responseC = $connector->sendAsync(new UserRequest)->wait();
@@ -43,6 +48,37 @@ test('the http events are fired when using the http sender with asynchronous eve
     expect($responseA->status())->toBe(200);
     expect($responseB->status())->toBe(200);
     expect($responseC->status())->toBe(200);
+
+    Event::assertDispatched(RequestSending::class, 3);
+    Event::assertDispatched(ResponseReceived::class, 3);
+});
+
+test('the http events are fired when using request pools', function () {
+    Config::set('saloon.default_sender', HttpSender::class);
+
+    Event::fake();
+
+    $connector = new HttpSenderConnector;
+
+    expect($connector->sender())->toBeInstanceOf(HttpSender::class);
+
+    $pool = $connector->pool([
+        'a' => new UserRequest,
+        'b' => new UserRequest,
+        'c' => new UserRequest,
+    ]);
+
+    $responses = [];
+
+    $pool->withResponseHandler(function (Response $response, string $key) use (&$responses) {
+        $responses[$key] = $response;
+    });
+
+    $pool->send()->wait();
+
+    expect($responses['a']->status())->toBe(200);
+    expect($responses['b']->status())->toBe(200);
+    expect($responses['c']->status())->toBe(200);
 
     Event::assertDispatched(RequestSending::class, 3);
     Event::assertDispatched(ResponseReceived::class, 3);
